f0bc02b98a4ab45048f87afeb54474398f818e97
= <=>
2024-10-07 15:21:55 -0300

    script modificado

--- script.sql
+++ script.sql
@@ -5,4 +5,12 @@ create table animal(
   nome_popular varchar(80) NOT NULL,
   nome_cientifico varchar(60) NOT NULL,
   habitos_noturnos LONGTEXT NOT NULL
-)
+);
+
+-- Continuando a aula 
+CREATE table avaliacao(
+   id int primary key auto_increment,
+   avaliacao text not null,
+   polaridade float not null
+);
+select * from avaliacao;
